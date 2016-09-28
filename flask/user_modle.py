d81ddf2d21207a3a4cdb848f42072207f9b1a217
liu-zzs <beifengzhishen2009@163.com>
2016-09-28 16:59:38 +0800

    默认的

---
+++ flask/user_modle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/9/27 0027 20:00
+# @Author  : zzs
+# @Site    :
+# @File    : user_modle.py
+# @Software: PyCharm
+
+class User(object):
+    def __init__(self,user_id,user_name):
+        self.user_id=user_id
+        self.user_name=user_name
+
+
+
+"""
+创建User这个类
+"""
